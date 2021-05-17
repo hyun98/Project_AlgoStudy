693f91c11caacf515ebbfff8913fabdc4bc54368
LJEDD2 <78216102+LJEDD2@users.noreply.github.com>
2021-05-17 21:32:45 +0900

    Add files via upload

--- study/team2/BOJ_2504_이정은.py
+++ study/team2/BOJ_2504_이정은.py
@@ -1,60 +1,44 @@
-'''
-https://www.youtube.com/watch?v=eu9ttD-psU4
-&list=PLDV-cCQnUlIYQOb8_n-d-VPhl_X6cECjg&index=1
-'''
-
 n = list(input())
-stack=[]
+stack = []
+result = []
+ans=0
 
-for i in range(len(n)):
-    if n[i] == ")": #닫는 괄호를 만났을 때 
-        sumv = 0
-        while stack:
-            top = stack.pop() # 맨 위의 괄호와 비교
-            if top == "(": #여는 괄호일 경우
-                if sumv == 0:
-                    stack.append(2) #2를 넣어준다 ()=2 
-                else:
-                    stack.append(2*sumv)
-                break
-            elif top == "[":
-                print(0)
-                exit(0)
-            else:
-                if sumv == 0 :
-                    sumv = int(top)
-                else:
-                    sumv = sumv + int(top)
-                    
-    elif n[i] == "]": #다른 닫는 괄호를 만났을 경우
-        sumv=0
-        while stack:
-            top = stack.pop()
-            if top == "[":
-                if sumv == 0 :
-                    stack.append(3)
-                else:
-                    stack.append(3*sumv)
-                break
-            elif top == "(": #짝이 안맞는 경우
-                print(0)
-                exit(0)
-            else:
-                if sumv == 0 :
-                    sumv = int(top)
-                else:
-                    sumv = sumv + int(top)
+for i in n:
+    if i == "(":
+        stack.append(i)
+        result.append("+2(")
+    elif i == "[":
+        stack.append(i)
+        result.append("+3(")
+            
+    elif i == ")":
+        if stack and stack[-1] == "(":
+            stack.pop()
+            result.append(")")
+        else:
+            result=[]
+            break
+        
+    elif i == "]":
+        if stack and stack[-1] == "[":
+            stack.pop()
+            result.append(")")
+
+        else:
+            result=[]
+            break
     else:
-        stack.append(n[i])
+        result=[]
+        break
 
-result = 0
-for i in stack:
-    if i == "(" or i == "[":
+if not stack:
+    if result:
+        result = "".join(result)
+        result=result.replace("()", "")
+        result=result.replace("(+","*(")
+        print(eval(result))
+    else:
         print(0)
-        exit(0)
-    else:
-        result += i
-
-
-print(result)
-                    
+else:
+    print(0)
+        
